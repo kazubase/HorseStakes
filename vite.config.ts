46048a96e0413fe0e67ecc5285081563ee30a518
kazu <kazu.baseballr.gmail.com>
2025-01-28 10:44:29 +0900

    ダークモード適用５

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -41,13 +39,6 @@ export default defineConfig({
     },
   },
   css: {
-    postcss: {
-      plugins: [
-        tailwindcss({
-          config: './tailwind.config.ts'
-        }),
-        autoprefixer()
-      ],
-    },
+    postcss: './postcss.config.js',
   },
 });
