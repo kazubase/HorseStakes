3d8fac0622c92a70ceb124c19170769d6e4171c9
kazu <kazu.baseballr.gmail.com>
2025-02-02 11:07:47 +0900

    distファイル削除

--- dist/client/src/components/ui/collapsible.jsx
+++
@@ -1,5 +0,0 @@
-import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
-var Collapsible = CollapsiblePrimitive.Root;
-var CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
-var CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
-export { Collapsible, CollapsibleTrigger, CollapsibleContent };
